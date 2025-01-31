8670682f3ccfb3d981509ff0da0a3b250e3b41d1
Samuel-Romaric <samuelromaric2015@gmail.com>
2025-01-31 16:34:39 +0000

    Fix

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -126,7 +126,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.1.8/b-3.2.0/b-colvis-3.2.0/b-html5-3.2.0/b-print-3.2.0/r-3.0.3/datatables.min.js"></script>
 <script>
-    // let update_sale_route = "{{ route('admin.sales.show', $item->id) }}";
+    // let update_sale_route = "{{-- route('admin.sales.show', $item->id) --}}";
 
     $(document).ready(function () {
         $('#Table').DataTable({
